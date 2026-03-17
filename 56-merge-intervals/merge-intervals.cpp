d5f186f1f007853f1ec01fc4a74365cef1f902f9
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-17 17:03:37 +0530

    Time: 2 ms (92.82%) | Memory: 23.8 MB (59.09%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,23 +5,24 @@ public:
         int n = arr.size();
 
         sort(arr.begin(), arr.end());
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int start = arr[i][0];
             int end = arr[i][1];
 
-            if(!ans.empty() && ans.back()[1]>= end){
+            if (!ans.empty() && ans.back()[1] >= end) {
                 continue;
             }
-          
-                for(int j = i+1; j<n; j++){
-                 if(arr[j][0] <=end){
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
                     end = max(end, arr[j][1]);
-                 }
-                
+                } else {
+                    break;
+                }
             }
             ans.push_back({start, end});
         }
 
-     return ans;
+        return ans;
     }
 };
