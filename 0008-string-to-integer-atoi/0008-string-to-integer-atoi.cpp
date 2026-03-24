3b1e4e8470336aef23603b34183b2d9353d782d5
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-24 10:05:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.length();
+        int i = 0;
+
+        if(i == n ) return 0;
+
+        while(i<n && s[i]==' ') {i++;}
+
+        int sign = 1;
+
+        if(s[i]== '-'){
+          sign = -1;
+          i++;
+        }
+        else if(s[i]== '+'){
+           
+            i++;
+        }
+
+        int ans = 0;
+        int MAX = INT_MAX;
+        int MIN = INT_MIN;
+
+        while(i<n && isdigit(s[i])){
+            int digit = s[i]-'0';
+            ans = ans*10+digit;
+
+           if(sign == 1 && ans > MAX ) return MAX;
+           if(sign == -1 && -ans < MIN ) return MIN;
+
+           i++;
+        }
+
+        return (int)(sign * ans);
+    }
+};
