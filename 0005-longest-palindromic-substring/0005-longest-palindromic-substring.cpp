d46dfa9c0da4f28802d01d24686bb4da5babef32
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-27 22:53:16 +0530

    Time: 203 ms (18.51%), Space: 13.7 MB (49.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
-class Solution {
+/* class Solution {
 public:
     string expand(string s, int left, int right, int n, string& ans) {
-        
+
         while (left >= 0 && right < n) {
             if (s[left] != s[right]) {
                 break;
@@ -25,5 +25,48 @@ public:
         }
 
         return ans;
+    }
+};
+
+*/
+
+class Solution {
+public:
+    bool solve(string& s, int i, int j, int t[][1001]) {
+
+        if (i >= j) {
+            return 1;
+        }
+
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
+
+        if (s[i] == s[j])
+            return t[i][j] = solve(s, i +1, j - 1, t);
+
+        else
+            return t[i][j] = 0;
+    }
+
+    string longestPalindrome(string s) {
+        int maxlen = 0;
+        int sp = 0;
+        int n = s.length();
+
+        int t[1001][1001];
+        memset(t, -1, sizeof(t));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j, t)) {
+                    if (j - i + 1 > maxlen) {
+                        maxlen = j - i + 1;
+                        sp = i;
+                    }
+                }
+            }
+        }
+        return s.substr(sp, maxlen);
     }
 };
