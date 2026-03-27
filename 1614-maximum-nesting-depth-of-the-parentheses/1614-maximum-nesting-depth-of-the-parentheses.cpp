912f28d287b780fcc422319f3077bbfa634f6dcb
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-27 15:00:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -4,7 +4,7 @@ public:
         int openBrackets = 0;
         int result = 0;
 
-        for (auto& ch : s) {
+        for (auto ch : s) {
             if (ch == '(') {
                 openBrackets++;
                 result = max(result, openBrackets);
