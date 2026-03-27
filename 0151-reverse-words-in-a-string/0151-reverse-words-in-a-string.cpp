44e853235a9f87b373da0a6a4af0f9221e615ae0
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-27 11:36:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,13 +1,41 @@
+/* Brute Force
+class Solution {
+public:
+   string reverseWords(string s) {
+       stringstream ss(s);
+       string token = "";
+       string result = "";
+
+       while(ss >> token){
+           result = token + " "+ result;
+       }
+       return result.substr(0, result.length()-1);
+   }
+};
+*/
+
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
-        string token = "";
-        string result = "";
+        reverse(s.begin(), s.end());
+        int i = 0;
+        int r = 0, l = 0;
+        int n = s.length();
 
-        while(ss >> token){
-            result = token + " "+ result;
+        while (i < n) {
+            while (i < n && s[i] != ' ') {
+                s[r++] = s[i++];
+            }
+
+            if (l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+                s[r] = ' ';
+                r++;
+                l = r;
+            }
+            i++;
         }
-        return result.substr(0, result.length()-1);
+        s = s.substr(0, r - 1);
+        return s;
     }
 };
