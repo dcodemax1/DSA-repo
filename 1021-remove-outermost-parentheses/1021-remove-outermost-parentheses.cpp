4e97bcf6dc9d1dca75ac9dea02d6de22e439ca3b
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-28 20:50:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.39%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,3 +1,5 @@
+//optimized  
+
 class Solution {
 public:
     string removeOuterParentheses(string s) {
@@ -5,7 +7,7 @@ public:
         string ans = "";
 
         for(int i = 0; i<s.length(); i++){
-            
+           
             if(s[i]==')') count--;
             if(count!= 0) ans.push_back(s[i]);
             if(s[i]=='(') count++;
