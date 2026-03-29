3b08b0f3f795ff28cfc5e9cf27d4b058c186b160
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-29 10:58:10 +0530

    Time: 47 ms (95.14%), Space: 8.8 MB (96.76%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -26,15 +26,13 @@ public:
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
-            int freq[26] ={0} ;
+            int freq[26] = {0};
             for (int j = i; j < n; j++) {
-                freq[s[j]-'a']++;
+                freq[s[j] - 'a']++;
 
-               beauty = getMaxCount(freq) - getMinCount(freq);
-               sum += beauty;
+                beauty = getMaxCount(freq) - getMinCount(freq);
+                sum += beauty;
             }
-             
-            
         }
         return sum;
     }
