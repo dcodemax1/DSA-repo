154273fb491a37e0a4f135774864ccfb47d9f98f
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-31 19:32:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,52 +8,49 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// Optimal Approach
+
 class Solution {
 public:
-     
-    ListNode* reverseHead(ListNode* head){
+    ListNode* reverseHead(ListNode* head) {
 
-        if(head == NULL || head->next == NULL ){
-           return head;
+        if (head == NULL || head->next == NULL) {
+            return head;
         }
 
-        ListNode* newHead = reverseHead(head->next);  
+        ListNode* newHead = reverseHead(head->next);
         ListNode* front = head->next;
         front->next = head;
-        head->next= NULL;
+        head->next = NULL;
 
         return newHead;
-
     }
 
     bool isPalindrome(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast->next != NULL && fast->next->next != NULL){
+        while (fast->next != NULL && fast->next->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
-
         }
         ListNode* newHead = reverseHead(slow->next);
         ListNode* first = head;
-        ListNode* second= newHead;
+        ListNode* second = newHead;
 
-        while(second != NULL){
-          if(first->val != second->val){
-            reverseHead(newHead);
-            return false;
-          }
-          first = first->next;
-          second = second->next;
-
+        while (second != NULL) {
+            if (first->val != second->val) {
+                reverseHead(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
         }
         reverseHead(newHead);
         return true;
-        
     }
 };
-
 
 /**
  * Definition for singly-linked list.
@@ -66,29 +63,29 @@ public:
  * };
  */
 
- // Brute Force 
- /*
+// Brute Force
+/*
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        stack<int> st;
+   bool isPalindrome(ListNode* head) {
+       stack<int> st;
 
-        ListNode* temp = head;
+       ListNode* temp = head;
 
-        while(temp != NULL){
-            st.push(temp->val);
-            temp = temp ->next;
-        }
+       while(temp != NULL){
+           st.push(temp->val);
+           temp = temp ->next;
+       }
 
-        temp = head;
-        while(temp != NULL){
-            if(temp->val != st.top()){
-                return false;
-            }
-            st.pop();
-            temp = temp->next;
-        }
-        return true;
-    }
+       temp = head;
+       while(temp != NULL){
+           if(temp->val != st.top()){
+               return false;
+           }
+           st.pop();
+           temp = temp->next;
+       }
+       return true;
+   }
 };
 */
