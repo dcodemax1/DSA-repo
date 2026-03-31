c9c1e34efe8d6b1d35b276be6f524f2762dd8a6d
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-31 23:04:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -8,36 +8,73 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- // Brute Force
+
+ // Optimall Approach
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        int cnt = 0;
-        ListNode* temp = head;
+        ListNode* slow = head;
+        ListNode* fast = head->next->next;
 
-        if (head == NULL || head->next == NULL)
-            return NULL;
+        if(head == NULL || head->next == NULL) return NULL;
 
-        while (temp != NULL) {
-            cnt++;
-            temp = temp->next;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int res = cnt / 2;
+        ListNode* middle = slow->next;
+        slow->next = slow->next->next;
+        delete middle;
 
-        temp = head;
-
-        while (temp != NULL) {
-            res--;
-
-            if (res == 0) {
-                ListNode* middle = temp->next;
-                temp->next = temp->next->next;
-                delete middle;
-                return head;
-            }
-            temp = temp->next;
-        }
         return head;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// Brute Force
+
+/*
+class Solution {
+public:
+   ListNode* deleteMiddle(ListNode* head) {
+       int cnt = 0;
+       ListNode* temp = head;
+
+       if (head == NULL || head->next == NULL)
+           return NULL;
+
+       while (temp != NULL) {
+           cnt++;
+           temp = temp->next;
+       }
+
+       int res = cnt / 2;
+
+       temp = head;
+
+       while (temp != NULL) {
+           res--;
+
+           if (res == 0) {
+               ListNode* middle = temp->next;
+               temp->next = temp->next->next;
+               delete middle;
+               return head;
+           }
+           temp = temp->next;
+       }
+       return head;
+   }
+};
+
+*/
