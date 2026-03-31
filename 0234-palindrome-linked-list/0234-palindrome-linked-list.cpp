4cbd050b278c2820909e51fc4215b7075fd24a2b
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-31 19:31:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,6 +10,66 @@
  */
 class Solution {
 public:
+     
+    ListNode* reverseHead(ListNode* head){
+
+        if(head == NULL || head->next == NULL ){
+           return head;
+        }
+
+        ListNode* newHead = reverseHead(head->next);  
+        ListNode* front = head->next;
+        front->next = head;
+        head->next= NULL;
+
+        return newHead;
+
+    }
+
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+        ListNode* newHead = reverseHead(slow->next);
+        ListNode* first = head;
+        ListNode* second= newHead;
+
+        while(second != NULL){
+          if(first->val != second->val){
+            reverseHead(newHead);
+            return false;
+          }
+          first = first->next;
+          second = second->next;
+
+        }
+        reverseHead(newHead);
+        return true;
+        
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ // Brute Force 
+ /*
+class Solution {
+public:
     bool isPalindrome(ListNode* head) {
         stack<int> st;
 
@@ -31,3 +91,4 @@ public:
         return true;
     }
 };
+*/
