0ba1c198c5941c45a8fbdb1a3240476c4645f96b
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-31 12:10:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,28 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- // Brute Force 
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int cnt = 0;
-        while(temp != NULL){
-            cnt++;
-            temp= temp->next;
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-
-       int  mid = (cnt)/2 +1;
-       temp = head;
-
-        while(temp != NULL){
-            mid--;
-
-            if(mid == 0 ) break;
-            temp = temp->next;
-        }
-
-        return temp;
+        return slow;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Brute Force
+
+/*
+class Solution {
+public:
+   ListNode* middleNode(ListNode* head) {
+
+       ListNode* temp = head;
+       int cnt = 0;
+
+       if(head == NULL || head->next == NULL) return head;
+
+       while(temp != NULL){
+           cnt++;
+           temp= temp->next;
+       }
+
+      int  mid = (cnt)/2 +1;
+      temp = head;
+
+       while(temp != NULL){
+           mid--;
+
+           if(mid == 0 ) break;
+           temp = temp->next;
+       }
+
+       return temp;
+   }
+};
+
+*/
