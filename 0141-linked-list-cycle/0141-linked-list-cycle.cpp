55d40a04ab64821ac78dcb2d05bc133b9cd3f765
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-03-31 15:06:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -7,8 +7,35 @@
  * };
  */
 
- 
+ // Optimal Solution 
 class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// brute Force
+
+/*class Solution {
 public:
     bool hasCycle(ListNode *head) {
         unordered_map<ListNode *, int> mp;
@@ -22,8 +49,10 @@ public:
             mp[temp] =1;
             temp= temp->next;
 
-            
+
         }
         return false;
     }
 };
+
+*/
