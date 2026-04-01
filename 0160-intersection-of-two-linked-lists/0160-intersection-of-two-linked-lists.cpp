0298a4548a515ab382c60e233eaa4410977447e6
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-01 11:03:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,48 +7,86 @@
  * };
  */
 
- // Better Approach
+// Optimal Approach
+
 class Solution {
-
-    ListNode* findCollision(ListNode* t1, ListNode* t2, int diff) {
-        while (diff) {
-            diff--;
-            t2 = t2->next;
-        }
-
-        while (t1 != t2) {
-            t1 = t1->next;
-            t2 = t2->next;
-        }
-        return t1;
-    }
-
 public:
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
         ListNode* t1 = headA;
         ListNode* t2 = headB;
-        int n1 = 0, n2 = 0;
 
-        while (t1 != NULL) {
-            n1++;
+        while (t1 != t2) {
+
             t1 = t1->next;
-        }
-
-        while (t2 != NULL) {
-            n2++;
             t2 = t2->next;
-        }
 
-        t1 = headA;
-        t2 = headB;
+            if (t1 == t2)
+                return t1;
 
-        if (n2 > n1) {
-            return findCollision(t1, t2, n2 - n1);
-        } else {
-            return findCollision(t2, t1, n1 - n2);
+            if (t1 == NULL)
+                t1 = headB;
+            if (t2 == NULL)
+                t2 = headA;
         }
+        return t1;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// Better Approach
+
+/*
+class Solution {
+
+   ListNode* findCollision(ListNode* t1, ListNode* t2, int diff) {
+       while (diff) {
+           diff--;
+           t2 = t2->next;
+       }
+
+       while (t1 != t2) {
+           t1 = t1->next;
+           t2 = t2->next;
+       }
+       return t1;
+   }
+
+public:
+   ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+       ListNode* t1 = headA;
+       ListNode* t2 = headB;
+       int n1 = 0, n2 = 0;
+
+       while (t1 != NULL) {
+           n1++;
+           t1 = t1->next;
+       }
+
+       while (t2 != NULL) {
+           n2++;
+           t2 = t2->next;
+       }
+
+       t1 = headA;
+       t2 = headB;
+
+       if (n2 > n1) {
+           return findCollision(t1, t2, n2 - n1);
+       } else {
+           return findCollision(t2, t1, n1 - n2);
+       }
+   }
+};
+
+*/
 
 /**
  * Definition for singly-linked list.
