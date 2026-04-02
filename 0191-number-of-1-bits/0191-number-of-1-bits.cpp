54fb4ae36b7573e837916cb6dee74b1d9ea74219
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-02 23:45:17 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.01%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        // return __builtin_popcount(n);
+        int cnt = 0;
+
+        while (n != 0) {
+
+            n = (n & n - 1);
+            cnt++;
+        }
+        return cnt;
+    }
+};
