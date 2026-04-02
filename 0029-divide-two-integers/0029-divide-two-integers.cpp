b0ae6f3e5a08f15b889034c282ce432d876fbe6f
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-03 01:49:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+
+        int sign = true;
+        if (dividend <= 0 && divisor > 0)
+            sign = false;
+        else if (divisor <= 0 && dividend > 0)
+            sign = false;
+
+        int n = abs(dividend);
+        int d = abs(divisor);
+
+        long ans = 0;
+
+        while (n >= d) {
+            int cnt = 0;
+            while (n >= (d << (cnt + 1))) {
+                cnt++;
+            }
+            ans += (1 << cnt);
+            n -= (d << cnt);
+        }
+
+        if (ans == 1 << 31 && sign)
+            return INT_MAX;
+        if (ans == 1 << 31 && !sign)
+            return INT_MIN;
+
+        return sign ? ans : -ans;
+    }
+};
