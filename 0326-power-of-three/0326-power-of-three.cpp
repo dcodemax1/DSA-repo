16e731e5a303718c379d83ddd5757c4ed78ce083
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-03 01:05:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        if(n==1) return true;
+        if(n<=0) return false;
+       return isPowerOfThree(n/3);
+    }
+};
+
+
+/*
+// Brute Force 
+    bool isPowerOfThree(int n) {
         if(n<=0) return false;
 
         while(n%3 == 0){
@@ -8,4 +18,4 @@ public:
         }
         return n==1;
     }
-};
+*/
