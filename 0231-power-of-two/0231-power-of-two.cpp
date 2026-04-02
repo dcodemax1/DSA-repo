7245b7321c4c583525687a8751529dbdf64f59a1
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-03 01:17:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-      /*  if(n <= 0)
-        return false;
+        /*  Optimal 
+        if(n <= 0)
+          return false;
 
-         return ((n & n - 1) == 0); 
-         */
+           return ((n & n - 1) == 0);
 
-         if(n<=0) return false;
+           */
+        if (n == 1)
+            return true;
+        if (n <= 0)
+            return false;
+
+        return n % 2 == 0 && isPowerOfTwo(n / 2);
+    }
+};
+
+/* Iterative Approach
+if(n<=0) return false;
 
          while(n%2 == 0){
             n/=2;
          }
          return n==1;
-         }
-};
+
+         */
