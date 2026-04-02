87c88460b4cbd422fe13ffadeedf2fe18c4852df
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-02 15:14:02 +0530

    Time: 7 ms (52.36%), Space: 15 MB (85.88%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -14,8 +14,70 @@ public:
 };
 */
 
-// Brute Force 
+class Solution {
+public:
+    void insertInBetween(Node* head) {
+        Node* temp = head;
+        while (temp != NULL) {
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = copyNode->next;
+        }
+    }
 
+    void connectRandom(Node* head) {
+        Node* temp = head;
+        while (temp != NULL) {
+            Node* copyNode = temp->next;
+            if (temp->random) {
+                copyNode->random = temp->random->next;
+            } else {
+                copyNode->random = NULL;
+            }
+            temp = temp->next->next;
+        }
+    }
+
+    Node* connectNext(Node* head) {
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+        Node* temp = head;
+
+        while (temp != NULL) {
+            res->next = temp->next;
+            temp->next = temp->next->next;
+            res = res->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+    }
+
+    Node* copyRandomList(Node* head) {
+        insertInBetween(head);
+        connectRandom(head);
+        return connectNext(head);
+    }
+};
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+// Brute Force
+/*
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
@@ -38,3 +100,4 @@ public:
         return mp[head];
     }
 };
+*/
