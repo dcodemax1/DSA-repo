4360f6925256f33a01bf96d323f73d04df0d4b3a
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-03 18:04:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
+// Optimal Approach
     vector<int> singleNumber(vector<int>& nums) {
-        map<int, int> mp;
+        int n = nums.size();
+
+        int xorr = 0;
+
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ nums[i];
+        }
+
+        int rightmostBit = (xorr & xorr - 1) ^ xorr;
+        int number1 = 0, number2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if ((nums[i] & rightmostBit)) {
+                number1 = number1 ^ nums[i];
+            } else {
+                number2 = number2 ^ nums[i];
+            }
+        }
+        if (number1 < number2)
+            return {number2, number1};
+        return {number1, number2};
+    }
+};
+
+/* Brute Force
+
+ map<int, int> mp;
         int n = nums.size();
         vector<int> ans;
 
@@ -17,5 +44,5 @@ public:
                 break;
         }
         return ans;
-    }
-};
+
+        */
