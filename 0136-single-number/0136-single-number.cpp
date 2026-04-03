e389cde4c0285f0656d5039d94976276ca9b0320
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-03 12:07:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int maxi = 0;
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int cnt = 0;
 
-            for (int j = 0; j < n; j++) {
-                if (nums[j] == nums[i]) {
-                    cnt++;
-                }
-            }
-            if (cnt == 1) {
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+        }
+
+        vector<int> hash(maxi+1,0);
+
+        for(int i = 0; i<n; i++){
+            hash[nums[i]]++;
+        }
+
+        for(int i = 0; i<n; i++ ){
+            if(hash[nums[i]]==1){
                 return nums[i];
             }
         }
         return -1;
     }
 };
+
+/*  Brure Force
+
+int n = nums.size();
+       for (int i = 0; i < n; i++) {
+           int cnt = 0;
+
+           for (int j = 0; j < n; j++) {
+               if (nums[j] == nums[i]) {
+                   cnt++;
+               }
+           }
+           if (cnt == 1) {
+               return nums[i];
+           }
+       }
+       return -1;
+
+       */
