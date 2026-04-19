d4200a1389968b79f19230e33ce29bc08d1643ae
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-19 17:46:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -17,7 +17,7 @@ public:
 
         int cnt = 0;
 
-        for (int i = 0; i < prime.size(); i++) {
+        for (int i = 2; i < n; i++) {
             if (prime[i] == 1) {
                 cnt++;
             }
