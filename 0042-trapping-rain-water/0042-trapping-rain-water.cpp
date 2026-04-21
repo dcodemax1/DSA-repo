f0f4cbceb015d5e5987b894a11c777bbf8efdeae
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-21 16:12:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,5 +1,41 @@
 class Solution {
 public:
+// Optimal Approach
+    int trap(vector<int>& height) {
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+        int n = height.size();
+        int l = 0;
+        int r = n - 1;
+
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (leftMax > height[l]) {
+                    total += leftMax - height[l];
+                } else {
+                    leftMax = height[l];
+                    
+                }
+                l++;
+            } else {
+                if (rightMax > height[r]) {
+                    total += rightMax - height[r];
+                } else {
+                    rightMax = height[r];
+                    
+                }
+                r--;
+            }
+        }
+        return total;
+    }
+};
+
+/*
+
+class Solution {
+public:
 
     vector<int> prefixMax(vector<int>& height){
         int n = height.size();
@@ -8,7 +44,7 @@ public:
         prefix[0] = height[0];
 
         for(int i = 1; i<n; i++){
-          prefix[i] = max(prefix[i-1], height[i]); 
+          prefix[i] = max(prefix[i-1], height[i]);
         }
         return prefix;
     }
@@ -19,7 +55,7 @@ public:
          suffix[n-1] = height[n-1];
 
         for(int i = n-2; i>=0; i--){
-          suffix[i] = max(suffix[i+1], height[i]); 
+          suffix[i] = max(suffix[i+1], height[i]);
         }
         return suffix;
     }
@@ -28,10 +64,10 @@ public:
     int trap(vector<int>& height) {
         int n= height.size();
         int total = 0;
-       
+
          vector<int> prefix = prefixMax(height);
          vector<int> suffix = suffixMax(height);
-        
+
         for(int i =0; i<n; i++){
             int leftMax = prefix[i];
             int rightMax = suffix[i];
@@ -43,3 +79,5 @@ public:
         return total ;
     }
 };
+
+*/
