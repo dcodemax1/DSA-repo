32df273b2c7762e213a8b821beffa3d6e17b70a4
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-22 00:11:28 +0530

    Time: 12 ms (91.01%), Space: 81.3 MB (80.18%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,7 +1,41 @@
 class Solution {
 public:
+// Optimal Approach 
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+        int nse;
+        int pse;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int element = st.top();
+                st.pop();
+
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
+                maxArea = max(maxArea, heights[element] * (nse - pse - 1));
+            }
+            st.push(i);
+        }
+        while (!st.empty()) {
+            nse = n;
+            int element = st.top();
+            st.pop();
+            pse = st.empty() ? -1 : st.top();
+
+            maxArea = max(maxArea, heights[element] * (nse - pse - 1));
+        }
+
+        return maxArea;
+    }
+};
+/*
+class Solution {
+public:
     // Brute Force
-    
+
     vector<int> findNSE(vector<int>& heights, int n){
      vector<int> nse(n);
      stack<int> st;
@@ -45,3 +79,5 @@ public:
         return maxi;
     }
 };
+
+*/
