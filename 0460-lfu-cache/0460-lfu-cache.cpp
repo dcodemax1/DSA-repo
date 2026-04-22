132405c12ee5e80a931bb04c57a0d968e128738f
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-22 23:30:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -0,0 +1,76 @@
+class LFUCache {
+public:
+    int cap;
+    int size;
+
+    unordered_map<int, list<vector<int>>::iterator> mp;
+    map<int, list<vector<int>>> freq;
+
+    LFUCache(int capacity) {
+        cap = capacity;
+        size = 0;
+    }
+
+void updateFreq(int key) {
+        auto& vec = *(mp[key]);
+
+        int value = vec[1];
+        int f = vec[2];
+
+        freq[f].erase(mp[key]);
+        if (freq[f].empty()) {
+            freq.erase(f);
+        }
+        f++;
+
+        freq[f].push_front({key, value, f});
+        mp[key] = freq[f].begin();
+
+    }
+
+    int get(int key) {
+        if (mp.find(key) == mp.end()) {
+            return -1;
+        }
+
+        auto vec = *(mp[key]);
+        int value = vec[1];
+        updateFreq(key);
+        return value;
+    }
+
+    void put(int key, int value) {
+        if (cap == 0)
+            return;
+        if (mp.find(key) != mp.end()) {
+            auto& vec = *(mp[key]);
+            vec[1] = value;
+            updateFreq(key);
+        }
+
+        if (size < cap) {
+            size++;
+            freq[1].push_front({key, value, 1});
+            mp[key] = freq[1].begin();
+        } else {
+            auto& kaun_sa_list = freq.begin()->second;
+            auto key_to_delete = (kaun_sa_list.back())[0];
+            kaun_sa_list.pop_back();
+
+            if (kaun_sa_list.empty()) {
+                freq.erase(freq.begin()->first);
+            }
+
+            freq[1].push_front({key, value, 1});
+            mp.erase(key_to_delete);
+            mp[key] = freq[1].begin();
+        }
+    }
+};
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
