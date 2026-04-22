ced1058d06a02e6d1814eda68429fe30fe6baada
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-22 10:21:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+// Optimal Approach
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> ls;
+
+        for(int i =0; i<n; i++){
+            if(!dq.empty() && dq.front() < i-k+1){
+                dq.pop_back();
+            }
+            
+            while(!dq.empty() && nums[dq.back()] <= nums[i]){
+                dq.pop_back();
+            }
+         
+         dq.push_back(i);
+
+         if(i>=k-1){
+            ls.push_back(nums[dq.front()]);
+         }
+        }
+        return ls;
+    }
+};
+
+/*
+class Solution {
+public:
+// Brute Force 
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxi = 0;
+        vector<int> ls;
+        for (int i = 0; i <= n - k; i++) {
+            maxi = nums[i];
+            for (int j = i; j <= i + k - 1; j++) {
+                maxi = max(maxi, nums[j]);
+            }
+            ls.push_back(maxi);
+        }
+        return ls;
+    }
+};
+
+*/
