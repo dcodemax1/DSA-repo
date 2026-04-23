3b9243c37bb228387db24b7de8141bb1d32c8845
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-23 23:26:10 +0530

    Time: 9 ms (51.77%), Space: 11.5 MB (41.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,33 +1,79 @@
 class Solution {
 public:
+
+// Optimal Approach 
+
     string minWindow(string s, string t) {
-        int n = s.size(), m = t.size();
-        int l = 0, r = 0, minlen = 1e9;
-        int sIndex = -1, cnt = 0;
-        int hash[256] = {0};
+        int sIndex = -1;
+        int minLen = INT_MAX;
+        int cnt = 0;
+        int n = s.length();
+        int m = t.length();
+        unordered_map<int,int> mp;
+        int l = 0, r= 0;
 
-        for (int i = 0; i < m; i++)
-            hash[t[i]]++;
-        while (r < n) {
-            if (hash[s[r]] > 0)
-                cnt++;
-            hash[s[r]]--;
+        for(int i =0; i<m; i++){
+            mp[t[i]]++;
+        }
 
-            while (cnt == m) {
-                if (r - l + 1 < minlen) {
-                    minlen = r - l + 1;
-                    sIndex = l;
-                    
-                }
-                hash[s[l]]++;
-                    if (hash[s[l]] > 0)
-                        cnt--;
-                        l++;
-                }
-                r++;
+        while(r<n){
+          if(mp[s[r]]>0){ cnt++;}
+          mp[s[r]]--;
+
+          while(cnt==m){
+            if(r-l+1 < minLen){
+                minLen = r-l+1;
+                sIndex = l;
             }
-        
+           
+            mp[s[l]]++;
+            if(mp[s[l]]> 0){
+                cnt--;
+            }
+             l++;
+          }
+          r++;
+        }
 
-        return sIndex == -1 ? "" : s.substr(sIndex, minlen);
+        return sIndex == -1 ? "" : s.substr(sIndex, minLen);
+
     }
 };
+
+/*
+class Solution {
+public:
+
+// Brute Force 
+
+    string minWindow(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        int minLen = INT_MAX;
+        int sIndex = -1;
+
+        for (int i = 0; i < n; i++) {
+            unordered_map<int, int> mp;
+            for (int j = 0; j < m; j++) {
+                mp[t[j]]++;
+            }
+            int cnt = 0;
+            for (int j = i; j < n; j++) {
+                if (mp[s[j]] > 0)
+                    cnt++;
+
+                mp[s[j]]--;
+
+                if (cnt == m) {
+                    if (j - i + 1 < minLen) {
+                        minLen = j - i + 1;
+                        sIndex = i;
+                    }
+                    break;
+                }
+            }
+        }
+        return sIndex == -1 ? "" : s.substr(sIndex, minLen);
+    }
+};
+*/
