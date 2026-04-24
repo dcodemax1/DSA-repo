58864f410704a3b82df493860b8ee37b7163230a
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-24 11:25:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size();
+        int m = s.size();
+        int cnt = 0;
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int l = 0, r = 0;
+
+        while (l < n && r < m) {
+            if (s[r] >= g[l]) {
+                cnt++;
+                l++;
+            }
+            r++;
+        }
+        return cnt;
+    }
+};
