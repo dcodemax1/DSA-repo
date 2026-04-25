64d4eb643e5685d4958cf6efd625f582d55f880d
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-04-26 00:35:07 +0530

    Time: 0 ms (100%), Space: 22.1 MB (84.4%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,8 +1,90 @@
 class Solution {
 public:
 
-// Brute Force 
+// Optimal Approach 
 
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        int sum = 1, i = 1;
+
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                sum += 1;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                sum += peak;
+
+                i++;
+            }
+            int down = 1;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if (peak < down) {
+                sum = sum + (down - peak);
+            }
+        }
+
+        return sum;
+    }
+};
+
+/*
+
+class Solution {
+public:
+    // Brute Force
+
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        vector<int> left(n);
+
+        left[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+
+            } else {
+                left[i] = 1;
+            }
+        }
+
+        int sum = max(1, left[n - 1]);
+
+        for (int i = n - 2; i >= 0; i--) {
+            int curr = 1, right = 1;
+
+            if (ratings[i] > ratings[i + 1]) {
+                curr = right + 1;
+                right = curr;
+            } else {
+                curr = 1;
+            }
+
+            sum = sum + (max(left[i], curr));
+        }
+
+        return sum;
+    }
+};
+
+*/
+
+/*
+class Solution {
+public:
+
+// Brute Force
     int candy(vector<int>& ratings) {
         int n = ratings.size();
 
@@ -15,7 +97,7 @@ public:
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 left[i] = left[i - 1] + 1;
-                
+
             } else {
                 left[i] = 1;
             }
@@ -36,3 +118,5 @@ public:
         return sum;
     }
 };
+
+*/
