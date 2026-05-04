f45de15c302cd7a5b38a1e1a01eb8d2154259e58
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-04 17:20:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,7 +1,69 @@
 class Solution {
 public:
 
-// Brute Force 
+//  Better Approach 
+
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int i = 0;
+        int j = 0;
+
+        int cnt = 0;
+        
+        int n = (n1 + n2);
+
+        int ind2 = (n / 2);
+        int ind1 = ind2 - 1;
+
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
+                if (cnt == ind1)
+                    ele1 = nums1[i];
+                if (cnt == ind2)
+                    ele2 = nums1[i];
+                cnt++;
+                i++;
+            } else {
+                if (cnt == ind2)
+                    ele2 = nums2[j];
+                if (cnt == ind2)
+                    ele2 = nums2[j];
+                cnt++;
+                j++;
+            }
+        }
+
+        while (i < n1) {
+            if (cnt == ind1)
+                ele1 = nums1[i];
+            if (cnt == ind2)
+                ele2 = nums1[i];
+            cnt++;
+            i++;
+        }
+
+        while (j < n2) {
+            if (cnt == ind2)
+                ele2 = nums2[j];
+            if (cnt == ind2)
+                ele2 = nums2[j];
+            cnt++;
+            j++;
+        }
+
+        if(n % 2 == 1) return ele2;
+
+        return (double)((double)(ele1+ele2))/2.0;
+    }
+};
+
+/*class Solution {
+public:
+
+// Brute Force
 
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> nums3;
@@ -32,10 +94,12 @@ public:
         if (n1 % 2 == 1)
             return nums3[n1 / 2];
 
-        return (double)((double)(nums3[n1 / 2]) + (double)(nums3[(n1 / 2) - 1])) /
-               2.0;
+        return (double)((double)(nums3[n1 / 2]) + (double)(nums3[(n1 / 2) - 1]))
+/ 2.0;
     }
 };
+
+*/
 
 /*
 class Solution {
