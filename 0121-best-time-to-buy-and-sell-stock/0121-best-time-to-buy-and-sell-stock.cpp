dc1b9844781f8c942ba970cd4fb69977db9d2dff
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-07 23:39:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        int mini = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < n; i++) {
+            mini = min(mini, prices[i]);
+            int cost = prices[i] - mini;
+            maxProfit = max(maxProfit, cost);
+        }
+        return maxProfit;
+    }
+};
