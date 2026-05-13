834b5a6be1f8abb0c7547f733d66b042be7f6f9b
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-13 16:05:56 +0530

    Time: 0 ms (100%), Space: 10.9 MB (92.34%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -10,6 +10,40 @@
  * right(right) {}
  * };
  */
+
+ // Iterative Approach using 2 stack 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postOrder;
+        if (root == NULL)
+            return postOrder;
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+
+        st1.push(root);
+
+        while (!st1.empty()) {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if (root->left != NULL)
+                st1.push(root->left);
+            if (root->right != NULL)
+                st1.push(root->right);
+            
+        }
+        while (!st2.empty()) {
+            postOrder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postOrder;
+    }
+};
+
+/*
+// recusrive traversal approach
+
 class Solution {
 public:
     void findpostOrder(TreeNode* root, vector<int>& res) {
@@ -27,3 +61,4 @@ public:
         return res;
     }
 };
+*/
