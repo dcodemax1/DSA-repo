2006584e0d0593c504e82dde898784f0d1fa26b0
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-20 15:31:26 +0530

    Time: 0 ms (100%), Space: 10.8 MB (72.21%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -10,6 +10,35 @@
  * right(right) {}
  * };
  */
+
+// Iterative Solution
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> pre;
+
+        if (root == NULL)
+            return pre;
+        stack<TreeNode*> st;
+
+        st.push(root);
+        while (!st.empty()) {
+            root = st.top();
+            st.pop();
+
+            pre.push_back(root->val);
+            if (root->right != NULL)
+                st.push(root->right);
+            if (root->left != NULL)
+                st.push(root->left);
+        }
+        return pre;
+    }
+};
+
+/* Brute Force Recursive Approach
+
 class Solution {
 public:
     void findPreOrder(TreeNode* root, vector<int>& pre){
@@ -33,3 +62,4 @@ preorderTraversal(TreeNode* root) {
   }
 };
 
+*/
