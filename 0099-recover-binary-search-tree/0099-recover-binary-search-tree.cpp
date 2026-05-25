dc850277fc3999a9fe28df8f1444dea91d1564e5
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-26 02:31:35 +0530

    Time: 3 ms (17.81%), Space: 60.9 MB (48.5%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -6,12 +6,58 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 
- // Brute Force 
  
+class Solution {
+
+private:
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last;
+
+private:
+    void inorder(TreeNode* root) {
+        if (root == NULL)
+            return;
+
+        inorder(root->left);
+
+        if (prev != NULL && root->val < prev->val) {
+
+            if (first == NULL) {
+                first = prev;
+                middle = root;
+            }
+
+            else {
+                last = root;
+            }
+        }
+        prev = root;
+        inorder(root->right);
+    }
+
+public:
+    void recoverTree(TreeNode* root) {
+
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+
+        if(first && last ) swap(first->val, last->val);
+
+        else if(first && middle) swap(first->val, middle->val);
+    }
+};
+
+/*
+ // Brute Force
+
 class Solution {
 public:
 
@@ -44,3 +90,5 @@ public:
         replaceValues(root, i, arr);
     }
 };
+
+*/
