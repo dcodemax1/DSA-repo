211341148d2793c0726d10be2e54738e7a0650eb
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-27 14:32:00 +0530

    Time: 40 ms (26.02%), Space: 61.4 MB (66.44%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
 
-// Better Approach 
-
+// Better 2nd Approach 
     int findKthLargest(vector<int>& nums, int k) {
-       priority_queue<int> pq;
+        int n = nums.size();
 
-       int n = nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-       for(int i = 0; i<n; i++){
-        pq.push(nums[i]);
-       }
+        for(int i = 0; i<n; i++){
+            pq.push(nums[i]);
 
-       for(int i = 1; i<k; i++){
-        pq.pop();
-       }
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+     return pq.top();
 
-       return pq.top();
     }
 };
 
 /*
-// Brute Force 
+// Better Approach
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            pq.push(nums[i]);
+        }
+
+        for (int i = 1; i < k; i++) {
+            pq.pop();
+        }
+
+        return pq.top();
+    }
+};
+*/
+
+/*
+// Brute Force
 
 class Solution {
 public:
@@ -33,4 +55,3 @@ public:
 };
 
 */
-
