d9be46fa8efb353f685933f8181fa2ce68f50dd7
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-27 12:40:54 +0530

    Time: 9 ms (99.1%), Space: 85.6 MB (92.46%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,7 +1,40 @@
 class Solution {
 public:
+
+// Better Approach 
+
     int distributeCandies(vector<int>& candyType) {
-        
+        sort(candyType.begin(), candyType.end());
+
+        int n = candyType.size();
+        int i = 0;
+        int j = 1;
+        int count = 1;
+
+        while (j < n) {
+            if (candyType[i] == candyType[j]) {
+                j++;
+            } else {
+                count++;
+                i = j;
+                j++;
+            }
+        }
+        if (count > n / 2) {
+            return n / 2;
+        }
+
+        return count;
+    }
+};
+
+/*
+// Brute Force
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+
         int n = candyType.size();
         set<int> s;
 
@@ -12,3 +45,5 @@ public:
         return min(n/2, (int)s.size());
     }
 };
+
+*/
