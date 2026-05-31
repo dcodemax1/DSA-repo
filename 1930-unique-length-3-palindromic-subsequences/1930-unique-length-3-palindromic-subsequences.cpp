67c780d8f3a59f0f712b85893554dc965141e886
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-05-31 14:52:11 +0530

    Time: 151 ms (57.27%), Space: 15.8 MB (61.62%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,7 +1,49 @@
 class Solution {
 public:
 
-// O(n) Better approach 
+// O(n) 2nd Better Approach with pre-computation 
+
+    int countPalindromicSubsequence(string s) {
+        vector<pair<int, int>> indices(26, {-1, -1});
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            int idx = ch - 'a';
+
+            if (indices[idx].first == -1) {
+                indices[idx].first = i;
+            }
+            indices[idx].second = i;
+        }
+
+        int result = 0;
+        
+        for (int i = 0; i < 26; i++) {
+            int left_idx = indices[i].first;
+            int right_idx = indices[i].second;
+
+            if (left_idx == -1)
+                continue;
+           
+            unordered_set<char> st;
+
+            for (int middle = left_idx + 1; middle <= right_idx - 1; middle++) {
+                st.insert(s[middle]);
+            }
+
+            result += st.size();
+        }
+
+        return result;
+    }
+};
+
+/*
+class Solution {
+public:
+
+// O(n) Better approach
 
     int countPalindromicSubsequence(string s) {
 
@@ -39,3 +81,5 @@ public:
         return result;
     }
 };
+
+*/
