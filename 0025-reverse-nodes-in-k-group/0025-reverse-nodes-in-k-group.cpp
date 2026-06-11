0fc7716df01b5d50ad8f44d02434c8bef194729d
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-06-11 23:21:57 +0530

    Time: 0 ms (100%), Space: 16.5 MB (38.11%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,58 +8,61 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-ListNode* findKthNode(ListNode* head, int k) {
-    ListNode* kth = head;
-    k--;
-
-    while (kth != NULL && k != 0) {
-        kth = kth->next;
-        k--;
-    }
-    return kth;
-}
-
-ListNode* reverseList(ListNode* head) {
-    if (head == NULL || head->next == NULL) {
-        return head;
-    }
-    ListNode* newHead = reverseList(head->next);
-
-    ListNode* front = head->next;
-    front->next = head;
-    head->next = NULL;
-
-    return newHead;
-}
-
 class Solution {
 public:
+    ListNode* findKthNode(ListNode* head, int k) {
+        ListNode* temp = head;
+        k--;
+        while (temp != NULL && k != 0) {
+            temp = temp->next;
+            k--;
+        }
+        return temp;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* newHead = reverseList(head->next);
+
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* temp = head;
         ListNode* prevNode = NULL;
 
         while (temp != NULL) {
             ListNode* kthNode = findKthNode(temp, k);
+
             if (kthNode == NULL) {
                 if (prevNode) {
                     prevNode->next = temp;
                     break;
                 }
             }
+
             ListNode* nextNode = kthNode->next;
-            kthNode->next = NULL;
+
+            kthNode->next = nullptr;
 
             ListNode* newHead = reverseList(temp);
+
             if (temp == head) {
                 head = newHead;
             } else {
                 prevNode->next = kthNode;
-                
             }
-            prevNode= temp;
-            temp= nextNode;
+
+            prevNode = temp;
+            temp = nextNode;
         }
+
         return head;
     }
 };
