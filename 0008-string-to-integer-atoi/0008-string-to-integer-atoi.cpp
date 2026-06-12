b85fde84f3116b6cd26727935e8ef700ba6d7ded
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-06-12 19:34:05 +0530

    Time: 0 ms (100%), Space: 10.2 MB (5.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,5 +1,52 @@
 class Solution {
 public:
+
+// Recursive Approach
+
+    int convert(string s, int n, int i, long ans , int sign ) {
+
+        if (i > n || !isdigit(s[i])) {
+            return sign * ans;
+        }
+
+        int digit = s[i] - '0';
+        ans = ans * 10 + digit;
+
+        int Maxi = INT_MAX, Mini = INT_MIN;
+
+        if (sign == 1 && sign * ans > Maxi)
+            return Maxi;
+        if (sign == -1 && sign * ans < Mini)
+            return Mini;
+
+      return convert(s, n, i + 1, ans, sign);
+    }
+
+    int myAtoi(string s) {
+        int n = s.length();
+
+        int i = 0;
+
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+
+        int sign = 1;
+
+        if (s[i] == '-') {
+            sign = -1;
+            i++;
+        } else if (s[i] == '+') {
+            i++;
+        }
+
+        return convert(s, n, i, 0, sign );
+    }
+};
+
+/*
+class Solution {
+public:
     int myAtoi(string s) {
         int n = s.length();
         int i = 0;
@@ -15,7 +62,7 @@ public:
           i++;
         }
         else if(s[i]== '+'){
-           
+
             i++;
         }
 
@@ -36,3 +83,5 @@ public:
         return (int)(sign * ans);
     }
 };
+
+*/
