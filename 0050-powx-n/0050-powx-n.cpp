1d613817d5cedc5859486b4bfc19e02e1b28088f
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-06-13 23:26:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (69.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,5 +1,41 @@
 class Solution {
 public:
+
+    double solve (double x, long long n1){
+
+       if(n1 == 0){
+        return 1.0;
+       }
+
+       double ans = solve(x, n1/2);
+
+       if(n1% 2 == 0){
+        return ans*ans;
+       }else {
+        return x*ans*ans;
+       }
+
+    }
+
+    double myPow(double x, int n) {
+         long long n1 = n;
+
+        if (n1 < 0) {
+            x = 1 / x;
+            n1 = -n1;
+        }
+
+
+        return solve(x, n1);
+    }
+};
+
+/*
+class Solution {
+public:
+
+// Iterative Approach 
+
     double myPow(double x, int n) {
         double ans = 1.0;
         if (x == 0)
@@ -32,3 +68,5 @@ public:
         return ans;
     }
 };
+
+*/
