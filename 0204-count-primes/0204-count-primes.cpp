0fc6fb178707eae13c8a1cc44c68f3b25718187c
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-06-15 11:25:10 +0530

    Time: 138 ms (94.09%), Space: 132.7 MB (8.12%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -7,7 +7,7 @@ public:
         vector<int> prime(n, 1);
         prime[0] = prime[1] = 0;
 
-        for (int i = 0; i * i < n; i++) {
+        for (int i = 2; i * i < n; i++) {
             if (prime[i] == 1) {
                 for (int j = i * i; j < n; j += i) {
                     prime[j] = 0;
@@ -16,6 +16,7 @@ public:
         }
 
         int cnt = 0;
+
         for (int i = 2; i < n; i++) {
             if (prime[i] == 1) {
                 cnt++;
