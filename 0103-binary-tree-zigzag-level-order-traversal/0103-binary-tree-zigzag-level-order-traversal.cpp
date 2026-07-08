8441216f90fcb9e1ea6bf19c592985740a3eccab
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-07-08 23:56:24 +0530

    Time: 0 ms (100%), Space: 15.2 MB (48%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,37 +13,38 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-
-        if (root == NULL)
-            return ans;
-        queue<TreeNode*> q;
-        bool leftToRight = true;
-
-        q.push(root);
-
-        while (!q.empty()) {
-
-            int size = q.size();
-            vector<int> res(size);
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-
-                int index = leftToRight ? i : (size - i - 1);
-
-                res[index] = node->val;
-
-                if (node->left != NULL)
-                    q.push(node->left);
-                if (node->right != NULL)
-                    q.push(node->right);
-            }
-            leftToRight = !leftToRight;
-            ans.push_back(res);
+        vector<vector<int>> result;
+        if (root == NULL){
+            return result;
         }
 
-        return ans;
+        queue<TreeNode*> nodesQueue;
+        nodesQueue.push(root);
+        bool leftToRight = true;
+
+        while (!nodesQueue.empty()) {
+            int size = nodesQueue.size();
+            vector<int> row(size);
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = nodesQueue.front();
+                nodesQueue.pop();
+
+                int index = (leftToRight) ? i : (size - 1 - i);
+                row[index] = node->val;
+
+                if (node->left) {
+                    nodesQueue.push(node->left);
+                }
+                if (node->right) {
+                    nodesQueue.push(node->right);
+                }
+            }
+                leftToRight = !leftToRight;
+
+                result.push_back(row);
+            }
+        
+        return result;
     }
 };
