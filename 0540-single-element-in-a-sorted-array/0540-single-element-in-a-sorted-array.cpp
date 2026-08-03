a841f5b4da1dea665d9fc5d287841cdae6d974bc
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-08-03 18:51:57 +0530

    Time: 0 ms (100%), Space: 26 MB (99.82%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -19,7 +19,7 @@ public:
                 return nums[mid];
             }
 
-            if (mid % 2 == 1 && nums[mid] == nums[mid - 1] ||
+            else if (mid % 2 == 1 && nums[mid] == nums[mid - 1] ||
                 mid % 2 == 0 && nums[mid] == nums[mid + 1]) {
                 low = mid + 1;
             } else {
