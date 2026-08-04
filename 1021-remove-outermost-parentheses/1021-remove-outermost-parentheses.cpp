dd6954a06834bf6cd1c02a1772af5e7484d4e568
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-08-04 17:58:05 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.84%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,18 +1,47 @@
-//optimized  
-
 class Solution {
 public:
+
+// Optimal Approach 
+
     string removeOuterParentheses(string s) {
-        int count =0;
+        int count = 0;
         string ans = "";
 
         for(int i = 0; i<s.length(); i++){
-           
             if(s[i]==')') count--;
-            if(count!= 0) ans.push_back(s[i]);
+            if(count !=0) ans.push_back(s[i]);
             if(s[i]=='(') count++;
-
+            
+            
         }
         return ans;
     }
 };
+
+
+/*
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char> st;
+        string ans = "";
+
+        for(auto ch : s){
+            if(ch == '('){
+                if(!st.empty()){
+                    ans+=ch;
+                }
+                st.push(ch);
+            }
+            else{
+                st.pop();
+                if(!st.empty()){
+                    ans+=ch;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+*/
