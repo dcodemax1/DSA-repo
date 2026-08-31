a533fbe18cc49d9ba881380b4ea1198f38c008ed
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-08-31 23:14:45 +0530

    Time: 491 ms (13.84%), Space: 8.8 MB (52.39%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
+   
+   bool ifSafe(int row, int col, char c, vector<vector<char>>& board){
 
-    bool isValid(int row, int col, char c, vector<vector<char>>& board){
-        for(int i = 0; i<9; i++ ){
+    for(int i = 0; i<9; i++){
+        if(board[i][col] == c) return false;
 
-            if(board[i][col]==c){
-                return false;
-            }
+        if(board[row][i] == c) return false;
 
-             if(board[row][i]==c){
-                return false;
-            }
-
-            if(board[3* (row/3) + i/3][3*(col/3) + i%3] ==c){
-                return false;
-            }
-              
-
-        }
-        return true;
-
+        if(board[3*(row/3)+i/3][3*(col/3)+i%3]== c) return false;
+        
     }
+    return true; 
+   }
+
 
     bool solve(vector<vector<char>>& board){
-       
-        for(int i = 0; i<board.size(); i++){
-            for(int j = 0; j<board[0].size(); j++){
-                if(board[i][j] =='.'){
+        int n = board.size();
+        int m = board[0].size();
 
-                    for(char c ='1'; c<='9'; c++){
-                        if(isValid(i,j,c,board)){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m ; j++){
+                if(board[i][j]=='.'){
+                    for(char c = '1'; c <='9' ; c++){
+                       if(ifSafe(i, j, c, board)){
                             board[i][j] = c;
-                           if(solve(board)== true)
-                           return true;
 
-                           else board[i][j] ='.';
+                            if(solve(board)==true) return true;
+                            else board[i][j] = '.';
                         }
                     }
                     return false;
