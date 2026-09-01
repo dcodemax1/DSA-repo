3077ed3e5698c9040b11d068b2dff3acc755d4c7
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-09-01 16:04:40 +0530

    Time: 0 ms (100%), Space: 14 MB (57.24%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-// Optimal Approach
     vector<int> singleNumber(vector<int>& nums) {
         int n = nums.size();
-
-        long xorr = 0;
+        long long xorr = 0;
 
         for (int i = 0; i < n; i++) {
-            xorr = xorr ^ nums[i];
+            xorr ^= nums[i];
         }
 
-        int rightmostBit = (xorr & xorr - 1) ^ xorr;
+        long long rightmost = (xorr & xorr - 1) ^ xorr;
         int number1 = 0, number2 = 0;
 
         for (int i = 0; i < n; i++) {
-            if ((nums[i] & rightmostBit)) {
-                number1 = number1 ^ nums[i];
+            if (nums[i] & rightmost) {
+                number1 ^= nums[i];
             } else {
-                number2 = number2 ^ nums[i];
+                number2 ^= nums[i];
             }
         }
-        if (number1 < number2)
+
+        if (number1 < number2) {
+            return {number1, number2};
+        } else {
             return {number2, number1};
-        return {number1, number2};
+        }
     }
 };
-
-/* Brute Force
-
- map<int, int> mp;
-        int n = nums.size();
-        vector<int> ans;
-
-        for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-        }
-
-        for (auto x : mp) {
-            if (x.second == 1) {
-                ans.push_back(x.first);
-            }
-            if (ans.size() == 2)
-                break;
-        }
-        return ans;
-
-        */
