eb16b6828a6a41c6dd25776ebddf7362a826ab44
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-09-05 15:54:14 +0530

    Time: 68 ms (28.07%), Space: 151 MB (93.2%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,54 +1,53 @@
 class MinStack {
-    stack<long long > st;
-    long long mini = INT_MAX;
-
 public:
-    MinStack() {}
 
-    void push(int val) {
-        if (st.empty()) {
-            mini = val;
-            st.push(val);
-        } else {
-            if (val >= mini) {
-                st.push(val);
-            } else {
-                st.push(2LL * val - mini);
-                mini = val;
+    stack<long long> st;
+    long long mini;
+    MinStack() {
+         mini = INT_MAX;
+    }
+    
+    void push(int value) {
+        if(st.empty()){
+            st.push(value);
+            mini = value;
+        }
+        else{
+            if(value < mini){
+                st.push(2LL*value-mini);
+                mini = value;
             }
+            else st.push(value);
         }
     }
-
+    
     void pop() {
-        if (st.empty())
-            return;
+        if(st.empty()) return;
 
-        long long x = st.top();
+        long long el = st.top();
         st.pop();
-        if (x < mini) {
-            mini = 2LL * mini - x;
+        if(el < mini ){
+            mini = 2LL*mini - el;
         }
-    }
 
+    }
+    
     int top() {
-        if (st.empty())
-            return -1;
-        long long x = st.top();
-        if (x >= mini)
-            return (int)x;
-        else
-            return (int)mini;
-    }
-
-    int getMin() { 
         if(st.empty()) return -1;
-        return (int)mini; }
+        long long el = st.top();
+        return (el<mini) ? mini : el;
+    }
+    
+    int getMin() {
+        if(st.empty()) return -1;
+        return mini;
+    }
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(val);
+ * obj->push(value);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
