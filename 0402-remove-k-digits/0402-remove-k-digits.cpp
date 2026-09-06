dac435cf8a7eb21c1bd11e4d97856d4129c75081
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-09-06 15:41:22 +0530

    Time: 6 ms (29.77%), Space: 11.4 MB (55.77%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -2,37 +2,39 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         int n = num.length();
-        string res = "";
         stack<char> st;
+        string res = "";
 
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && k > 0 && st.top() - '0' > num[i] - '0') {
+        for(int i = 0; i<n; i++){
+            while(!st.empty()&& k>0 && st.top()-'0' > num[i]-'0'){
                 st.pop();
                 k--;
             }
             st.push(num[i]);
         }
+            while( k >0){
+                st.pop();
+                k--;
+            }
 
-        while (k > 0) {
+            if(st.empty()){
+                return "0";
+            }
+        
+
+        while(!st.empty()){
+            res+=st.top();
             st.pop();
-            k--;
-        }
-        if (st.empty())
-            return "0";
-
-        while (!st.empty()) {
-            res += st.top();
-            st.pop();
         }
 
-        while (res.size() != 0 && res.back() == '0') {
+        while(!res.empty() && res.back()=='0'){
             res.pop_back();
         }
-
         reverse(res.begin(), res.end());
 
-        if (res.empty())
+        if(res.empty()){
             return "0";
+        }
 
         return res;
     }
