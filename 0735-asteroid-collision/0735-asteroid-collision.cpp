45c1837ffa64ee7b19f3517f12fa66ff13354430
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-09-06 15:14:07 +0530

    Time: 4 ms (14.49%), Space: 21.6 MB (78.51%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> st;
+
         int n = asteroids.size();
 
         for (int i = 0; i < n; i++) {
@@ -15,7 +16,9 @@ public:
 
                 if (!st.empty() && st.back() == abs(asteroids[i])) {
                     st.pop_back();
-                } else if (st.empty() || st.back() < 0) {
+                }
+
+                else if (st.empty() || st.back() < 0) {
                     st.push_back(asteroids[i]);
                 }
             }
