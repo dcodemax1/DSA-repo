171e4ca545578bd6cce53d8bf914c345ef783b83
Deepika Kumari <deepikakumari@shooliniuniversity.com>
2026-09-14 22:21:43 +0530

    Time: 0 ms (100%), Space: 34.3 MB (51.29%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -12,55 +12,50 @@
  */
 class Solution {
 public:
-    TreeNode* findLastChild(TreeNode* root) {
 
-        if (root->right == NULL) {
-            return root;
-        }
+    TreeNode* findLastChild(TreeNode* root){
+      if(root->right == NULL){
+        return root;
+      }
 
-        return findLastChild(root->right);
+      return findLastChild(root->right);
     }
 
-    TreeNode* helper(TreeNode* root) {
-        if (root->left == NULL) {
+    TreeNode* helper(TreeNode* root){
+        if(root->left == NULL){
             return root->right;
         }
-
-        if (root->right == NULL) {
+        else if(root->right== NULL){
             return root->left;
         }
 
         TreeNode* rightChild = root->right;
-
         TreeNode* lastChild = findLastChild(root->left);
-
         lastChild->right = rightChild;
 
         return root->left;
     }
 
+
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == NULL)
-            return NULL;
+        if (root == NULL) {
+            return root;
+        }
 
         if (root->val == key) {
             return helper(root);
         }
         TreeNode* dummy = root;
-
         while (root != NULL) {
             if (root->val > key) {
                 if (root->left != NULL && root->left->val == key) {
                     root->left = helper(root->left);
                     break;
+                } else {
+                    root = root->left;
                 }
-             else {
-                root = root->left;
-            }
-            }
-
-            else {
-                if (root->right != NULL && root->right->val == key) {
+            } else {
+                if (root->right != NULL && root->right->val== key) {
                     root->right = helper(root->right);
                     break;
                 } else {
@@ -68,7 +63,6 @@ public:
                 }
             }
         }
-
         return dummy;
     }
 };
